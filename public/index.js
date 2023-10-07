1b7c84667059f3f21a7038c57d9620232810e731
218022429 <ngwenyabrendeline@gmail.com>
2023-10-07 16:24:48 +0200

    initial commit

---
+++ public/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+const app = express();
+
+app.get('/longestWord/:sentence', (req, res) => {
+  const sentence = req.params.sentence;
+  const words = sentence.split(' ');
+  let longestWord = '';
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }} res.send(longestWord);
+
+});
+console.log(app.get('/longestWord/:sentence'));
+app.get('/shortestWord/:sentence', (req, res) => {
+  const sentence = req.params.sentence;
+  const words = sentence.split(' ');
+  let shortestWord = words[0];
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length < shortestWord.length) {
+      shortestWord = words[i];
+    }} res.send(shortestWord);
+
+});
+console.log(app.get('/shortestWord/:sentence'));
+app.get('/wordLengths/:sentence', (req, res) => {
+  const sentence = req.params.sentence;
+  const words = sentence.split(' ');
+  let sum = 0;
+  for (let i = 0; i < words.length; i++) {
+    sum += words[i].length;
+  } res.send(sum.toString());
+});
+
+<div class="airtime" x-data=" {
+      usage : '',
+      availableAirtime : 0,
+      airtimeMessage : '',
+  
+      calculateAirtime() {
+  
+          axios.get(`/api/enough?usage=${this.usage}&availableAirtime=${this.availableAirtime}`)
+          .then(result => {
+              if (result.data.error) {
+                  alert (result.data.error);
+          } else {
+              this.airtimeMessage = result.data.totalBill;    }}
+          )}
+             
+      }"></div>
+      
+console.log(app.get('/wordLengths/:sentence'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));                                                                                                                                                                                                                                                                                                                                                                   
